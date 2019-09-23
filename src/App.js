2bb6ea44914d0463876e09fcf20ce721f1c2671d
jundymek <jundymek@gmail.com>
2019-09-23 18:57:40 +0200

    feat: MainHeader component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import MainHeader from './components/MainHeader';
 
 function App() {
   return (
     <div className="App">
-     Dupa
+      <MainHeader />
     </div>
   );
 }
---
+++ src/components/MainHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainHeader() {
+    return (
+      <header>
+        <h1>Moje repozytoria githuba - React</h1>
+      </header>
+    );
+  }
+
+export default MainHeader;
