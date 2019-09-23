c027594d3f0b647e955557c29bc072e5070b8891
jundymek <jundymek@gmail.com>
2019-09-23 19:54:43 +0200

    feat: SearchBar styles and sstate

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -5,13 +5,16 @@ import SearchBar from './SearchBar';
 const StyledHeader = styled.header`
     display: flex;
     background-color: #ccc;
-    padding: 20px;
+    padding: 40px 20px;
     justify-content: space-between;
+    align-items: center;
 `;
 
 const StyledH1 = styled.h1`
     font-weight: 700;
     font-size: 2.5rem;
+    margin: 0;
+    padding: 0;
 `;
 
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledInput = styled.input`
+    height: 2.5rem;
+    width: 20rem;
+    padding: .5rem;
+    border: none;
+`
+const StyledForm = styled.form`
+    display: flex;
+    flex-wrap: nowrap;
+`
+
+const StyledButton = styled.button`
+    height: 2.5rem;
+    padding: .5rem;
+    border: none;
+    background-color: gray;
+    color: #fff;
+    width: 5rem;
+`
 class SearchBar extends React.Component {
     constructor(props) {
         super(props)
@@ -8,12 +27,22 @@ class SearchBar extends React.Component {
             inputValue: ''
         }
     }
+
+    handleInputChange = (e) => {
+        console.log(e.target.value)
+        this.setState({inputValue: e.target.value})
+    }
+
     render () {
         return (
-            <input 
-                type="text" 
-                placeholder="Szukaj repo"
-            />
+            <StyledForm>
+                <StyledInput 
+                    type="text" 
+                    placeholder="Szukaj repo"
+                    onChange={this.handleInputChange}
+                />
+                <StyledButton>Szukaj</StyledButton>
+            </StyledForm>
         )
     }
 }
