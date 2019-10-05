2591fc8b8e086d8ea89a94a95702d4bb2e6a4720
jundymek <jundymek@gmail.com>
2019-10-05 12:28:00 +0200

    Updates

---
+++ precache-manifest.98e6ffcc8541fe94511a3bebab55df74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5d9b8946745c5629ea7a1b44911d32",
+    "url": "/react-github/index.html"
+  },
+  {
+    "revision": "d7f9e0d11e9ff7136a7b",
+    "url": "/react-github/static/css/main.fdc8a987.chunk.css"
+  },
+  {
+    "revision": "5d21ccaabf2674a06604",
+    "url": "/react-github/static/js/2.aa976d61.chunk.js"
+  },
+  {
+    "revision": "d7f9e0d11e9ff7136a7b",
+    "url": "/react-github/static/js/main.73f33458.chunk.js"
+  },
+  {
+    "revision": "39fec0a37a6459527658",
+    "url": "/react-github/static/js/runtime-main.9d59146e.js"
+  },
+  {
+    "revision": "5399464ec3db67f6f4748c0e3e423e99",
+    "url": "/react-github/static/media/code.5399464e.jpg"
+  },
+  {
+    "revision": "30561e1c19f3ef76ee5b83ae95993141",
+    "url": "/react-github/static/media/code1.30561e1c.jpg"
+  },
+  {
+    "revision": "a7f7797807505e69abb11637f1c27d96",
+    "url": "/react-github/static/media/code2.a7f77978.jpg"
+  },
+  {
+    "revision": "121d63bb46998903f943a509f8a75ebd",
+    "url": "/react-github/static/media/computer.121d63bb.jpg"
+  },
+  {
+    "revision": "cec17e167a418e95804779d5bf8b1c49",
+    "url": "/react-github/static/media/github_icon_small.cec17e16.png"
+  }
+]);
